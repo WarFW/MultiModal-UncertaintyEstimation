b8b2084348fdfb52cddb986099fe94901d905ac2
WarFW <106368492+WarFW@users.noreply.github.com>
2021-10-24 22:34:47 +0000

    readme.md updated.

--- ambiguous_cp/analysis.py
+++ ambiguous_cp/analysis.py
@@ -280,4 +280,5 @@ if UNCERTAIN_HIST:
         # Calculate plausibility scores
         plausibility_point = calib_plausibility_score_arr_np[i][:-1]
         plausibility_point = plausibility_point/np.sum(plausibility_point)
-     
+        max_at = np.argmax(plausibility_point)
+        plausibility_expected_scores.append(np.dot(calib_sim_score_arr_np[i], plausibility_point)
