006a9144dbb17fddaf0b2544103358ee98bcb053
WarFW <106368492+WarFW@users.noreply.github.com>
2022-01-12 08:48:25 +0000

    Update algorithm.md

--- ambiguous_cp/experiment.py
+++ ambiguous_cp/experiment.py
@@ -19,4 +19,8 @@ sys.path.append(base_path + '\\utils')
 from pets_classes import PETS_CLASSES, PETS_GENERIC_CLASSES
 from fitz17k_classes import FITZ17K_CLASSES, FITZ17K_GENERIC_CLASSES
 from medmnist_classes import MEDMNIST_CLASSES, MEDMNIST_GENERIC_CLASSES
-from confo
+from conformal_prediction_methods import *
+from metrics import *
+
+#Parse Arguments
+#-----------------------------------------------------------------
