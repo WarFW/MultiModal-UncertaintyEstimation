28cf606c60d5da9d76056ef7440db0c69ae0ede0
WarFW <106368492+WarFW@users.noreply.github.com>
2022-11-15 16:00:07 +0000

    Backup.

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -1,2 +1,5 @@
 import numpy as np
-from scipy.optimize i
+from scipy.optimize import brentq
+from sklearn.metrics import precision_score, recall_score
+import torch
+from torch.distributions.one_hot_categorical import OneHotCategori
