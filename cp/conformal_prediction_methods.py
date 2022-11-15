acf4df0094c4544a71e845c4473b4740759c7e60
WarFW <106368492+WarFW@users.noreply.github.com>
2022-11-15 18:19:19 +0000

    rwsem: do not block readers at head of queue if other readers are active

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -2,4 +2,8 @@ import numpy as np
 from scipy.optimize import brentq
 from sklearn.metrics import precision_score, recall_score
 import torch
-from torch.distributions.one_hot_categorical import OneHotCategori
+from torch.distributions.one_hot_categorical import OneHotCategorical
+from torch.distributions.categorical import Categorical 
+# BEGIN CONFORMAL RISK CONTROL
+'''
+For each sample, returns ratio of numb
