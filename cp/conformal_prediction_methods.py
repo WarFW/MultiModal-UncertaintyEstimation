62c3c4966a88b2adee5a01a381b907f2575ff5da
WarFW <106368492+WarFW@users.noreply.github.com>
2022-11-15 22:38:50 +0000

    kl

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -6,4 +6,6 @@ from torch.distributions.one_hot_categorical import OneHotCategorical
 from torch.distributions.categorical import Categorical 
 # BEGIN CONFORMAL RISK CONTROL
 '''
-For each sample, returns ratio of numb
+For each sample, returns ratio of number of false negatives to the number of expected true labels for that sample.
+Inputs:
+    - prediction_set_arr:   a boolean array represent
