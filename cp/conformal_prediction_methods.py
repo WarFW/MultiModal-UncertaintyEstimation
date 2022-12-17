09535582c4d68fc080dd77cb29c6a50ce8be02e4
WarFW <106368492+WarFW@users.noreply.github.com>
2022-12-17 22:48:50 +0000

    common changes

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -46,4 +46,6 @@ Inputs:
     - true_class_arr:       a boolean array representing the true classes
                                 for each image.
                             dim: (num_examples, num_classes)
-       
+                            element at (example_i, class_j) represents
+                                whether class_j is in the prediction set of
+     
