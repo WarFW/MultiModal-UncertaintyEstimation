2882e0ae61b296af9f974a67d9485da54acf0749
WarFW <106368492+WarFW@users.noreply.github.com>
2023-01-08 17:14:55 +0000

    Fix build break.

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -93,4 +93,9 @@ Inputs:
                                 P(class_j | example_i)
 Ouptut:
     a boolean array representing the coverage sets predicted for each image.
-        dim: (num_examples, num_class
+        dim: (num_examples, num_classes
+        element at (example_i, class_j) represents 
+            whether class_j is in the prediction set of
+            example_i.
+'''
+# END CON
