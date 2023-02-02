93d5f5e3e8896e55fc1bb761aebffdaeb4ba5825
WarFW <106368492+WarFW@users.noreply.github.com>
2023-02-02 12:55:55 +0000

    Simplification

--- cp/conformal_prediction_methods.py
+++ cp/conformal_prediction_methods.py
@@ -152,4 +152,7 @@ def set_size_inference_tpr_fpr(conformal_set_arr: np.array, true_class_arr: np.a
 # END SET SIZE CALIBRATION / INFERENCING
 
 # AMBIGUOUS CONFORMAL PREDICTION
-from torch.distributions.one_hot_categorical imp
+from torch.distributions.one_hot_categorical import OneHotCategorical
+from torch.distributions.categorical import Categorical 
+
+def monte_carlo_cp_deprecated(predictions: t
