9e5469a494d629380c878379850773e59f80a919
WarFW <106368492+WarFW@users.noreply.github.com>
2023-03-05 12:02:28 +0000

    Optimalization.

--- cp/conformal_risk_control.py
+++ cp/conformal_risk_control.py
@@ -1,2 +1,5 @@
 import numpy as np
-from scipy.optimi
+from scipy.optimize import brentq
+
+'''
+For each sample, returns ratio of number of false negatives to the number of expected true labels for that sample.
