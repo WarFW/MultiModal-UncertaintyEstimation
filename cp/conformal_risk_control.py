42cbb9f47f34dcacc441e153d167248dcdc4a2ae
WarFW <106368492+WarFW@users.noreply.github.com>
2023-03-05 20:07:36 +0000

    search added

--- cp/conformal_risk_control.py
+++ cp/conformal_risk_control.py
@@ -5,4 +5,6 @@ from scipy.optimize import brentq
 For each sample, returns ratio of number of false negatives to the number of expected true labels for that sample.
 Inputs:
     - prediction_set_arr:   a boolean array representing the coverage sets 
-                                predicted for 
+                                predicted for each image.
+                            dim: (num_examples, num_classes)
+                            element at (example_i, class_
