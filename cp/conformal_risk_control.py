3c9adf47b10f17f33a27fd34ff557ccba1555d03
WarFW <106368492+WarFW@users.noreply.github.com>
2023-03-11 09:44:20 +0000

    Linux 3.4.110

--- cp/conformal_risk_control.py
+++ cp/conformal_risk_control.py
@@ -14,4 +14,7 @@ Inputs:
                                 for each image.
                             dim: (num_examples, num_classes)
                             element at (example_i, class_j) represents
-                     
+                                whether class_j is in the prediction set of
+                                example_i.
+Output:
+    an array of length (true_class_a
