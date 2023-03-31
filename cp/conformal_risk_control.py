4ca31aaf7dd57be1db9ca0aba54580d18ce0898d
WarFW <106368492+WarFW@users.noreply.github.com>
2023-03-31 13:18:36 +0000

    Added newrelic

--- cp/conformal_risk_control.py
+++ cp/conformal_risk_control.py
@@ -42,4 +42,6 @@ Inputs:
                                 P(class_j | example_i)
     - true_class_arr:       a boolean array representing the true classes
                                 for each image.
-                   
+                            dim: (num_examples, num_classes)
+                            element at (example_i, class_j) represents
+                                whether clas
