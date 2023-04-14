d0cb1318d5a5ede21b41e20f1fc914a1c9e7fb08
WarFW <106368492+WarFW@users.noreply.github.com>
2023-04-14 12:56:05 +0000

    Text fixes

--- cp/metrics.py
+++ cp/metrics.py
@@ -1,2 +1,6 @@
 import numpy as np
-from
+from conformal_prediction_methods import LOSS_FUNCTION
+'''
+Coverage and Efficiency Metrics:
+Note that there are two universal arguments:
+    - prediction_set_arr:   a bo
