2995a90f3e967246619c5e231df7aa588b658f5a
WarFW <106368492+WarFW@users.noreply.github.com>
2023-04-14 22:36:13 +0000

    Bump eslint from 7.25.0 to 7.26.0

--- cp/metrics.py
+++ cp/metrics.py
@@ -3,4 +3,6 @@ from conformal_prediction_methods import LOSS_FUNCTION
 '''
 Coverage and Efficiency Metrics:
 Note that there are two universal arguments:
-    - prediction_set_arr:   a bo
+    - prediction_set_arr:   a boolean array representing the coverage sets 
+                                predicted for each image.
+                            dim: (n
