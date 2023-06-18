bf12db137a8686c41ca346d731ee4280dcb5d128
WarFW <106368492+WarFW@users.noreply.github.com>
2023-06-18 20:21:43 +0000

    :arrow_up: 1.1.2 :tada:

---
+++ data_mining/data_mining.py
@@ -0,0 +1,13 @@
+
+# # define search params
+# # option for commonly used search param are shown below for easy reference.
+# # For param marked with '##':
+# #   - Multiselect is currently not feasible. Choose ONE option only
+# #   - This param can also be omitted from _search_params if you do not wish to define any value
+# _search_params = {
+#     'q': '...',
+#     'num': 10,
+#     'fileType': 'jpg|gif|png',
+#     'rights': 'cc_publicdomain|cc_attribute|cc_sharealike|cc_noncommercial|cc_nonderived',
+#     'safe': 'active|high|medium|off|safeUndefined', ##
+#     'imgType': 'clipart|face|lineart|stock|photo|animated|imgTypeUndefined', ##
