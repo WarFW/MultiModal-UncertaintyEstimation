9563958cba2859d475d73dfddcfeaf89d05db89f
WarFW <106368492+WarFW@users.noreply.github.com>
2023-07-15 18:11:53 +0000

    -pedantic

--- image_caption_scraper/expansion.py
+++ image_caption_scraper/expansion.py
@@ -2,4 +2,11 @@ from nltk.corpus import wordnet
 from nltk import word_tokenize, pos_tag
 import itertools
 from selenium import webdriver
-from selenium.webdriver.chro
+from selenium.webdriver.chrome.options import Options
+import time
+from loguru import logger
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wo
