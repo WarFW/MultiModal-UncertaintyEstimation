088a5867b1e62615cf5a71ba866e1d7a0687fbf1
WarFW <106368492+WarFW@users.noreply.github.com>
2023-07-16 13:15:41 +0000

    s390/time: fix sched_clock() overflow

--- image_caption_scraper/expansion.py
+++ image_caption_scraper/expansion.py
@@ -13,4 +13,10 @@ def get_wordnet_pos(treebank_tag):
     elif treebank_tag.startswith('V'):
         return wordnet.VERB
     elif treebank_tag.startswith('N'):
-        return wordnet.
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+def generate_synonyms(phrase,k
